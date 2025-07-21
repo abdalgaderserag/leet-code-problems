51fdd3772bd4aadfe4bdea77e55bcdf62b1aae6c
Abd Al-Gader Sirag <38109544+abdalgaderserag@users.noreply.github.com>
2025-07-21 08:14:36 -0700

    maximum_time

--- src/problems/latest_time_by_replacing_hidden_digits.rs
+++ src/problems/latest_time_by_replacing_hidden_digits.rs
@@ -1,56 +1,39 @@
 pub fn solve() {
-    let result = maximum_time("2?:?0".to_owned());
+    let result = maximum_time("??:3?".to_owned());
     println!("{:?}", result);
 }
 fn maximum_time(time: String) -> String {
-    let mut i = 0;
-    let mut skip = false;
-    let mut yo: bool = false;
+    let mut i: i8 = -1;
     time.chars()
         .map(|c| {
+            i += 1;
             if c == '?' {
-                let mut ch: char;
                 if i == 0 {
-                    if let Some(second_char) = time.chars().nth(1) {
-                        if second_char == '?' {
-                            return '2';
-                        }
-                        let second = second_char.to_digit(10).expect("nah");
-                        if second == 9 {
-                            return '0';
-                        } else if second > 4 {
-                            return '1';
-                        }
+                    let ch = time.chars().nth(1).expect("shii");
+                    if ch == '?' {
                         return '2';
                     } else {
-                        ch = '2';
+                        let n = ch.to_digit(10).expect("nahh");
+                        if n > 3 {
+                            return '1';
+                        } else {
+                            return '2';
+                        }
                     }
                 } else if i == 1 {
-                    if let Some(char) = time.chars().nth(0) {
-                        let second = char.to_digit(10).expect("nah");
-                        if second == 2 {
-                            return '4';
-                        }
-                        return '9';
+                    let ch = time.chars().nth(0).expect("shii");
+                    if ch == '2' || ch == '?' {
+                        return '3';
                     } else {
-                        ch = '2';
+                        return '9';
                     }
-                } else if i == 1 && yo {
-                    ch = '3';
                 } else if i == 3 {
-                    ch = '5';
+                    return '5';
                 } else {
-                    ch = '9';
+                    return '9';
                 }
-                i += 1;
-                return ch;
-            } else {
-                if c == '2' && i == 0 {
-                    yo = true;
-                }
-                i += 1;
-                return c;
             }
+            return c;
         })
         .collect()
 }
